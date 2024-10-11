7f2b7c8bd4eaf196d6cc5c55a274b8b8396dbbf5
Nzi <167908919+Sachoni@users.noreply.github.com>
2024-10-11 12:25:45 +0300

    _menu.js

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -24,7 +24,7 @@ bot.addCommand(
     })
     const [date, time] = getDate()
     let CMD_HELP = `╭────────────────╮
-						ʟᴇᴠᴀɴᴛᴇʀ
+						NZI
 ╰────────────────╯
 
 ╭────────────────
@@ -93,7 +93,7 @@ bot.addCommand(
       }
     })
     const [date, time] = getDate()
-    let msg = `\`\`\`╭═══ LEVANTER ═══⊷
+    let msg = `\`\`\`╭═══ NZI-MD ═══⊷
 ┃❃╭──────────────
 ┃❃│ Prefix : ${PREFIX}
 ┃❃│ User : ${message.pushName}
