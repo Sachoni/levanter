c6b9bd497e0410f6ef3e228cca482426f50e4860
Nzi <167908919+Sachoni@users.noreply.github.com>
2024-10-12 12:51:28 +0300

    poll.js

--- plugins/poll.js
+++ plugins/poll.js
@@ -9,6 +9,7 @@ bot(
   async (message, match) => {
     const poll = match.split(',')
     if (poll.length < 3) return await message.send('*Example : question,option1,option2,...*')
+    const LOGO = "https://github.com/Sachoni/levanter/blob/master/lib/class/Weather.jpg";
     const name = poll[0]
     const options = []
     for (let i = 1; i < poll.length; i++) options.push({ optionName: poll[i] })
